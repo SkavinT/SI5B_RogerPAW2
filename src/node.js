c0da7996fb37756f8aac7fa8d107285c1e7e8c38
SkavinT <160827586+SkavinT@users.noreply.github.com>
2024-09-12 12:42:12 +0700

    Update node.js

--- src/node.js
+++ src/node.js
@@ -6,7 +6,11 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.get('/about', (req, res) => {
+    res.send('Halaman Tentang Perusahaan')
+});
   
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
