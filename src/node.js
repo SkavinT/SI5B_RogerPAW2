8691caa678e6a96b927eaf68cba4b5b460c2e092
SkavinT <160827586+SkavinT@users.noreply.github.com>
2024-09-12 13:00:26 +0700

    Update node.js

--- src/node.js
+++ src/node.js
@@ -1,16 +1,22 @@
-const express = require('express')
+const express = require('express');
 
-const app= express();
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.get('/about', (req, res) => {
-    res.send('Halaman Tentang Perusahaan')
+    res.send('Hello World!');
 });
-  
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
 
+app.get('/about', (req, res) => {
+    res.send('Halaman Tentang Perusahaan');
+});
+
+// middleware
+app.use('*', (req, res) => {
+    res.status(404);
+    res.send("404, Halaman tidak ditemukan");
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port 3000`);
+});
