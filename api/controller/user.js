37044c262232227af0482451961daa4e2531f173
SkavinT <160827586+SkavinT@users.noreply.github.com>
2024-10-24 12:58:19 +0700

    login

--- api/controller/user.js
+++ api/controller/user.js
@@ -1,20 +1,43 @@
 const User = require('../model/user');
+const bcrypt = require('bcrypt');
 
-const signUp = (req,res)=>{
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password,
-    });
-    user.save()
-    .then((result)=>{
-        res.status(201).json({
-            message: 'User berhasil dibuat',
+const signUp = (req, res) => {
+    bcrypt.hash(req.body.password, 10)
+        .then((hash) => {
+            const user = new User({
+                email: req.body.email,
+                password: hash,
+            });
+            user.save()
+                .then((result) => {
+                    res.status(201).json({
+                        message: 'User berhasil dibuat',
+                        // result: result,
+                    });
+                })
+                .catch((err) => {
+                    res.status(500).json({
+                        message: 'Internal Server Error',
+                        // error: err,
+                    });
+                });
         });
-    })
-    .catch((err)=>{
-        res.status(500).json({
-            message: 'Internal Server Error',
-        });
-    });
 
 };
+const login = (req, res) => {
+    let fetchedUser;
+    User.findOne({ 
+        email : req.body.email,
+    })
+    .then((user) => {
+        if(!user){
+            return res.status(401).json({
+                message: 'Autentikasi gagal',
+            });
+        }
+        fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password);
+        
+    });
+};
+module.exports = { signUp, login };
