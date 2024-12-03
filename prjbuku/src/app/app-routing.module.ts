490a55a6f99c602d6854f36d836b7ec7d48fd166
SkavinT <160827586+SkavinT@users.noreply.github.com>
2024-12-03 15:10:52 +0700

    error

--- prjbuku/src/app/app-routing.module.ts
+++ prjbuku/src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BukuComponent } from './buku/buku.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path : 'admin',
     component : AdminComponent,
     children :[
+      {
+        path : '/register',
+        component : RegisterComponent
+    
+      },
+    
       {
         path: '',
         component : DashboardComponent
