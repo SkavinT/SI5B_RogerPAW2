469357b4cdad48aae95f956e5fa75a757a95f215
SkavinT <160827586+SkavinT@users.noreply.github.com>
2024-12-05 13:10:14 +0700

    register validation

---
+++ prjbuku/src/app/models/user.model.ts
@@ -0,0 +1,7 @@
+export interface User {
+    _id : string | null,
+    // nama : string,
+    email : string,
+    password : string,
+    // role : string
+}
--- prjbuku/src/app/register/register.component.ts
+++ prjbuku/src/app/register/register.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'] // Fixed typo: changed 'styleUrl' to 'styleUrls'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  submitted: boolean = false;
+  executeState: string = "";
+  showSpinner = false;
+  private registerSub: Subscription = new Subscription();
 
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]] // Fixed missing comma
+    });
+  }
+  onsSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+    this.showSpinner = true;
+    this.executeState = "Loading...";
+    // Add your code here
+  }
 }
