70d64713f69327529298b4ee647c7e995f773e47
SkavinT <160827586+SkavinT@users.noreply.github.com>
2024-12-24 14:15:44 +0700

    Update buku.service.ts

--- prjbuku/src/app/services/buku.service.ts
+++ prjbuku/src/app/services/buku.service.ts
@@ -8,8 +8,8 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class BukuService {
-  //private url : string ="http://localhost:3000/buku/";
-  private url : string ="https://apisi51.vercel.app/buku/";
+  private url : string ="http://localhost:3000/buku/";
+  // private url : string ="https://apisi51.vercel.app/buku/";
 
   private subjectBuku = new Subject<Buku[]>();
   private subjectExexute = new Subject<string>;
